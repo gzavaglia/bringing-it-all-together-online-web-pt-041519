d7cef5225cbe969e339442105f3491b1cc9f3cbd
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-06-28 23:23:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -69,5 +69,16 @@ class Dog
     hash = {:id => row[0], :name => row[1], :breed => row[2]}
     self.new(hash)
   end
+  
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ? 
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+    
+  end
 
 end
