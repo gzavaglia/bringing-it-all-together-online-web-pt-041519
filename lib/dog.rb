60f54071550639d5a9ab5ddfcab2dee3a0c677fe
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-06-28 23:21:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,6 +2,7 @@ class Dog
   attr_accessor :name, :breed, :id
   def initialize(arguments)
     arguments.each {|key, value| self.send(("#{key}="), value)}
+    self.id ||= nil
   end
   
   def self.create_table
